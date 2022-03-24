92965d9dcfb52a82b0fb95c678d9f845b585aa02
Rui <ruimcpires@gmail.com>
2022-03-24 17:28:19 +0000

    Add Meals components

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={styles.summary}>
+      <h2>Delicious food!</h2>
+      <p>
+        Choose your favorite meal from our broad selection of available meals
+        and enjoy.
+      </p>
+      <p>
+        Choose your favorite meal from our broad selection of available meals
+        and enjoy.
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
