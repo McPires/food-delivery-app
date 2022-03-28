517fc0f250b9ca23c72f8a011abe6fd8624463be
Rui <ruimcpires@gmail.com>
2022-03-28 16:58:00 +0100

    Add useContext

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
-function App() {
+const App = () => {
+  const [isCartVisible, setIsCartVisible] = useState(false);
+
+  const showCartHandler = () => {
+    setIsCartVisible(true);
+  };
+
+  const hideCartHandler = () => {
+    setIsCartVisible(false);
+  };
+
   return (
-    <>
-      <Cart />
-      <Header />
+    <CartProvider>
+      {isCartVisible && (
+        <Cart onCloseModal={hideCartHandler} onClose={hideCartHandler} />
+      )}
+      <Header onShowCart={showCartHandler} />
       <Meals />
-    </>
+    </CartProvider>
   );
-}
+};
 
 export default App;
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
-import styles from "./HeaderCartButton.module.css";
+import classes from "./HeaderCartButton.module.css";
 
-const HeaderCartButton = () => {
+const HeaderCartButton = (props) => {
+  const cartCTX = useContext(CartContext);
+
+  const numberOfCartItems = cartCTX.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+
   return (
-    <button className={styles.button}>
-      <span className={styles.icon}>
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
